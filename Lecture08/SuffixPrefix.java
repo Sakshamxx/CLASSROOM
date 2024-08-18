597340c3eecb4f8ded0d0329d13e4089584c7af4
RYUGAA <sakshamchauhan003@gmail.com>
2024-08-19 02:30:14 +0530

    Leetcode Ques

---
+++ Lecture08/SuffixPrefix.java
@@ -0,0 +1,10 @@
+package Lecture08;
+
+public class SuffixPrefix {
+    public static void main(String[] args){
+        System.out.print("Suffix and Prefix Arrays.\r\n" +
+                        "LeetCode \r\n" +
+                        "Ques 1: 1991\r\n" +
+                        "Ques 2: 1423");
+    }
+}
